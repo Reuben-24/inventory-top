fdb80f7224616cc954afcf23ba37c3d1756b916b
Reuben-24 <reubengfaltiska@gmail.com>
2025-07-22 01:49:06 +1000

    Fix database_url env

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: `postgresql://${process.env.DB_USER}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
